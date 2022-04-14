393f30616f2e2d1e501bab2f474bdf7f0ae81659
Irfan Gazel <alpergazel@gmail.com>
2022-04-14 13:54:01 -0400

    ignore

--- src/EigthJavaClass.java
+++ src/EigthJavaClass.java
@@ -1,6 +1,10 @@
 public class EigthJavaClass {
-    System.out.println("hello");
-    
-    int x = 0;
-    int y+0;
+    public static void main(String[] args) {
+
+
+        System.out.println("hello");
+
+        int x = 0;
+
+    }
 }
